aa966e1565497a1622c10339b5b5e294a67864e4
Ubuntu <ubuntu@ip-172-31-44-64.eu-west-1.compute.internal>
2020-02-11 18:59:14 +0000

    Javascript advent listening and game building

--- assets/JavaScript/main.js
+++ assets/JavaScript/main.js
@@ -10,5 +10,36 @@ var gamePlay = false;
 var runButton = document.getElementById('run'); 
 var layOut = document.getElementById('layout');
 var message = document.getElementById('message');
+
 //event listens
 runButton.addEventListener('click', runGame); 
+function runGame() {
+    cardFlipped = -1;
+    playLockout = false;
+    runButton.style.display = 'none';
+    if (!gamePlay) {
+        gamePlay = true;
+        buildArray();
+        windowArray = windowImages.concat(windowImages);
+        shuffleArray(windowArray);
+        buildBoard();
+        message.innerHTML = "Click any window";
+    }
+}
+
+
+function buildArray() {
+  for (var x = 1; x < 21; x++) {
+    windowImages.push(x + '.jpg');
+  }
+}
+
+// game windows built using green image back with question mark
+function buildBoard() {
+  var html = "";
+  for (var x = 0; x <= (windowArray.length - 1); x++) {
+    html += '<div class="layOut"><div class="layOut">'; 
+    html += '<img id="' + x + '" src="images/back 3.jpg" onclick="pickCard(' + x + ',this)" class="flipImage"></div></div>';
+  }
+  layout.innerHTML = html;
+}
